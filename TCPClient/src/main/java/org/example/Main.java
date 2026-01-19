630c29e25b7f133151c9c91adb7acc7988154eb9
alimamedzada <alimamedzada@gmail.com>
2026-01-19 17:02:51 +0400

    read changes

--- TCPClient/src/main/java/org/example/Main.java
+++ TCPClient/src/main/java/org/example/Main.java
@@ -1,15 +1,30 @@
 package org.example;
 
 import java.io.DataOutputStream;
+import java.io.IOException;
 import java.io.OutputStream;
 import java.net.Socket;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        Socket socket = new Socket("192.168.100.3", 6789);
+    public static void main(String[] args) throws IOException {
+        writeAsByte();
+    }
+
+    public static void writeAsString() throws IOException {
+        Socket socket = new Socket("192.168.100.35", 6789);
         OutputStream os = socket.getOutputStream();
         DataOutputStream dos = new DataOutputStream(os);
         dos.write("Salam Server, necesen?".getBytes());
+        socket.close();
+    }
+
+    public static void writeAsByte() throws IOException {
+        Socket socket = new Socket("192.168.100.35", 6789);
+        OutputStream os = socket.getOutputStream();
+        DataOutputStream dos = new DataOutputStream(os);
+        byte[] bytes = NioFileUtility.readBytes("C:\\Users\\Asus\\Desktop\\me.jpg");
+        dos.write(bytes);
+        dos.flush();
         socket.close();
     }
 }
--- TCPServer/src/main/java/org/example/Main.java
+++ TCPServer/src/main/java/org/example/Main.java
@@ -6,16 +6,34 @@ import java.net.Socket;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+        readAsString();
+    }
+
+    public static void readAsString() throws IOException {
         ServerSocket ourFirstSocket = new ServerSocket(6789);
         while (true) {
             System.out.println("yeni socket veya yeni mushteri uchun gozleyirem...");
             Socket socket = ourFirstSocket.accept();
             System.out.println("Urra, yeni mushteri geldi");
             InputStream is = socket.getInputStream();
-            InputStreamReader isr = new InputStreamReader(is);
-            BufferedReader bis = new BufferedReader(isr);
-            String clientSentence = bis.readLine();
-            System.out.println("mushteri deyir ki:  " + clientSentence);
+            InputStreamReader bis  = new InputStreamReader(is);
+            BufferedReader br= new BufferedReader(bis);
+            String clientMessage = bis.getEncoding();
+            System.out.println("mushteri deyir ki:  " + clientMessage);
+        }
+    }
+
+    public static void readAsByte() throws IOException {
+        ServerSocket ourFirstSocket = new ServerSocket(6789);
+        while (true) {
+            System.out.println("yeni socket veya yeni mushteri uchun gozleyirem...");
+            Socket socket = ourFirstSocket.accept();
+            System.out.println("Urra, yeni mushteri geldi");
+            InputStream is = socket.getInputStream();
+            byte[] bytes = is.readAllBytes();
+            System.out.println("length = " + bytes.length);
+            NioFileUtility.writeBytes("C:\\Users\\Asus\\Desktop\\client.jpg", bytes);
+            System.out.println("length last= " + bytes.length);
         }
     }
 }
